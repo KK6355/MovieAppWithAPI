42286f3ae35fdb43ead918a7f92f217fad412a95
KK6355 <kangjiawei666@126.com>
2022-10-07 14:04:58 +1300

    Added a function that user can customiza the movie render with API call

---
+++ Operations/StaticList.cs
@@ -0,0 +1,7 @@
+﻿namespace MovieAppWithAPI.Operations
+{
+    public static class StaticList
+    {
+        public static List<string> RenderedMovies = new List<string>();
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,17 +4,18 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using MovieAppWithAPI.Services;
 using MovieAppWithAPI.Models;
 using Microsoft.EntityFrameworkCore;
-
+using MovieAppWithAPI.Operations;
 namespace MovieAppWithAPI.Pages
 {
     [BindProperties]
     public class IndexModel : PageModel
     {
-        //private readonly ILogger<IndexModel> _logger;
+      
         private readonly IAPI _api;
         public List<Movie> MovieList { get; set; }
+        public string IMDBId { get; set; } = "";
         public List<string> IMDBIdList { get; set; }
-        //public string IMDBId = "tt0245429";
+      
         public IndexModel(IAPI api)
         {
             _api = api;
@@ -23,7 +24,8 @@ namespace MovieAppWithAPI.Pages
             {
                 "tt0137523",
                 "tt0118799",
-                "tt0245429"
+                //"tt0245429",
+                //"tt0816692"
             };
         }
 
@@ -57,6 +59,41 @@ namespace MovieAppWithAPI.Pages
         
             return Page();
 
+
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+
+            if (IMDBId != null)
+            {
+                StaticList.RenderedMovies.Add(IMDBId);
+            }
+            IMDBIdList.AddRange(StaticList.RenderedMovies);
+            
+            foreach (var IMDBId in IMDBIdList)
+            {
+                Movie movie = new Movie();
+                Movie movieAPI = await _api.GetMovies(IMDBId, "9adad78f3fmsh65443a93600444dp17223ejsn9ecd90763758");
+                if (movieAPI != null)
+                {
+
+                    movie.Title = movieAPI.Title;
+                    movie.Image = movieAPI.Image;
+                    movie.Description = movieAPI.Description;
+
+                }
+                else
+                {
+                    movie.Title = "Invail API";
+                    movie.Image = "Invail API";
+                    movie.Description = "Invail API";
+                }
+                MovieList.Add(movie);
+
+            }
+            MovieList = MovieList.OrderBy(x => x.Title).ToList();
+
+            return Page();
 
         }
     }
